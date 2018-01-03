a5090a5215a9e5ae1aec59e607e6fe806cecb93f
Joy Hauser <joy.hauser@hotmail.com>
2018-01-03 10:06:13 -0600

    Add login page to server side

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  
-  // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+	res.json([{
+		id: 1,
+		name: "Joy Hauser",
+		ok: true
+	},
+	{
+		id: 2,
+		name: "Test User",
+		ok: true
+	}]);
 });
 
 module.exports = router;
