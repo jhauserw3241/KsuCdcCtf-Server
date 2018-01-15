104ce2ed8907291c8dd431ea3ac3917271fffdec
Joy Hauser <joy.hauser@hotmail.com>
2018-01-14 21:03:10 -0600

    Create SQL script to insert dummy challenges

---
+++ db/insertChallenges.sql
@@ -0,0 +1,18 @@
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Flash Drive Fun', 1, 'W234SDFWERIUD1',  10, 'This is the first challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Wheres My WiFi?', 2, 'W234SDFWERIUD2',  10, 'This is the second challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Simple Web Stuff', 3, 'W234SDFWERIUD3',  10, 'This is the third challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('File Fun', 4, 'W234SDFWERIUD4',  10, 'This is the fourth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Dandy Database', 5, 'W234SDFWERIUD5',  10, 'This is the fifth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Cant Crack This', 6, 'W234SDFWERIUD6',  10, 'This is the sixth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('/Pleasant/Path', 7, 'W234SDFWERIUD7',  10, 'This is the seventh challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Perfect Passwd', 8, 'W234SDFWERIUD8',  10, 'This is the eighth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Cant Crack This 2', 9, 'W234SDFWERIUD9',  10, 'This is the nineth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Forensic Fun', 10, 'W234SDFWERIUD10',  10, 'This is the tenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Ominous OpenVPN', 11, 'W234SDFWERIUD11',  10, 'This is the eleventh challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Get Smashed', 12, 'W234SDFWERIUD12',  10, 'This is the twelfeth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('More Smashed', 13, 'W234SDFWERIUD13',  10, 'This is the thirteenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Smashed with a Catch', 14, 'W234SDFWERIUD14',  10, 'This is the fourteenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('This is probably the hardest challenge15', 15, 'W234SDFWERIUD',  10, 'This is the fifteenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Ludicrous Lock', 16, 'W234SDFWERIUD16',  10, 'This is the sixteenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Love LUKS <3', 17, 'W234SDFWERIUD17',  10, 'This is the seventeenth challenges');
+INSERT INTO challenges(name, num, flag, points, clue) VALUES('Wireless Fiction', 18, 'W234SDFWERIUD18',  10, 'You are done');
