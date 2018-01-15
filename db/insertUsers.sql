bc227379a14c64eeddbead017db25c6a4f9cacda
Joy Hauser <joy.hauser@hotmail.com>
2018-01-14 21:03:25 -0600

    Create SQL script to create dummy users

---
+++ db/insertUsers.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(eid, score, suspended) VALUES('loganprough', 40, false);
+INSERT INTO users(eid, score, suspended) VALUES('jhauser', 30, false);
+INSERT INTO users(eid, score, suspended) VALUES('flemingcaleb', 20, false);
+INSERT INTO users(eid, score, suspended) VALUES('richardp', 10, false);
