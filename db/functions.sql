7a43497304bea5f5318afcabce5763322a3a66be
Logan <loganprough@ksu.edu>
2018-01-19 12:32:50 -0600

    Fixed behavior when user completes last challenge

--- db/functions.sql
+++ db/functions.sql
@@ -4,9 +4,6 @@ returns varchar as $$ begin
 if (select suspended from users where eid = seid) = 't' then
   return 'Account suspended.';
 end if;
-if not exists (select * from challenge_user where eid = seid) then
-  insert into challenge_user (eid, challenge) values (seid, (select name from challenges where num = 1));
-end if;
 if exists (select * from challenges where name = (select challenge from challenge_user where eid = seid) and flag = sflag) then
   update challenge_user set challenge = (select name from challenges where num = ((select num from challenges where flag = sflag) + 1)) where eid = seid;
   update users set score = score + (select points from challenges where flag = sflag) where eid = seid;
@@ -23,8 +20,12 @@ language sql;
 
 -- Get name of challenge a user with a given eid is on
 create or replace function currentName(varchar)
-returns varchar as 'select challenge from challenge_user where eid = $1;'
-language sql;
+returns varchar as $$ begin
+if ((select currentNum($1)) != (select max(num) from challenges)) then
+  return (select challenge from challenge_user where eid = $1);
+end if;
+return ''; end;
+$$ language plpgsql;
 
 -- Function to check if user exists and add if they don't
 create or replace function addUser(varchar) returns void as $$ begin
